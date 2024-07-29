1c6076b4e13b1960fece6b3e1fb165a8c06daebf
Alexey <alexeychamploo@gmail.com>
2024-07-29 13:36:16 +0500

    Fix forms

--- js/modules/forms.js
+++ js/modules/forms.js
@@ -1,11 +1,7 @@
-import { closeModal, openModal } from "./modal";
-import { postData } from "../services/services";
-
-function forms(formSelector, modalTimerId) {
+function forms() {
     // Forms
 
-    const forms = document.querySelectorAll(formSelector);
-
+    const forms = document.querySelectorAll('form');
     const message = {
         loading: 'img/form/spinner.svg',
         success: 'Спасибо! Скоро мы с вами свяжемся',
@@ -16,19 +12,32 @@ function forms(formSelector, modalTimerId) {
         bindPostData(item);
     });
 
+    const postData = async (url, data) => {
+        let res = await fetch(url, {
+            method: "POST",
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: data
+        });
+    
+        return await res.json();
+    };
+
+
+
     function bindPostData(form) {
         form.addEventListener('submit', (e) => {
             e.preventDefault();
 
-            const statusMessage = document.createElement('img');
+            let statusMessage = document.createElement('img');
             statusMessage.src = message.loading;
             statusMessage.style.cssText = `
                 display: block;
                 margin: 0 auto;
             `;
-            form.append(statusMessage);
             form.insertAdjacentElement('afterend', statusMessage);
-
+        
             const formData = new FormData(form);
 
             const json = JSON.stringify(Object.fromEntries(formData.entries()));
@@ -50,39 +59,24 @@ function forms(formSelector, modalTimerId) {
         const prevModalDialog = document.querySelector('.modal__dialog');
 
         prevModalDialog.classList.add('hide');
-        openModal('.modal', modalTimerId);
+        openModal();
 
         const thanksModal = document.createElement('div');
-        thanksModal.classList.add('modal__dialog')
+        thanksModal.classList.add('modal__dialog');
         thanksModal.innerHTML = `
             <div class="modal__content">
                 <div class="modal__close" data-close>×</div>
                 <div class="modal__title">${message}</div>
             </div>
         `;
-
         document.querySelector('.modal').append(thanksModal);
         setTimeout(() => {
             thanksModal.remove();
             prevModalDialog.classList.add('show');
             prevModalDialog.classList.remove('hide');
-            closeModal('.modal');
+            closeModal();
         }, 4000);
     }
-
-    // fetch('https://jsonplaceholder.typicode.com/posts', {
-    //     method: "POST",
-    //     body: JSON.stringify({name: 'Alex'}),
-    //     headers: {
-    //         'Content-type': 'application/json'
-    //     }
-    // })
-    //   .then(response => response.json())
-    //   .then(json => console.log(json));
-
-    fetch('http://localhost:3000/menu')
-        .then(data => data.json())
-        .then(res => console.log(res));
 }
 
-export default forms;
+module.exports = forms;
